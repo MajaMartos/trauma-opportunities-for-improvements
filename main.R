6e86c279cb654f9451b1dd3033d25716701fbd33
Maja Martos <majam@moil.noacs.io>
2023-02-01 12:02:30 +0100

    minor edits

--- main.R
+++ main.R
@@ -4,3 +4,5 @@ names <- c("swetrau","fmp","atgarder","problem","kvalgranskning2014.2017")
 names(data) <- names
 combined.dataset <- rofi::merge_data(data)
 combined.dataset$ofi <- rofi::create_ofi(combined.dataset)
+
+mean.age<- round(mean(combined.dataset[,"pt_age_yrs"]), digits = 2)
