fea5ed9768f012228561f96b2649867d87ebed77
Maja Martos <majam@noacs.io>
2023-02-14 14:31:15 +0100

    vet inte vad de 50 olika ASI-koderna står. Börjar gärna skapa cohorter tillsammans med jonatan

--- Fulkod.R
+++ Fulkod.R
@@ -3,11 +3,11 @@ library(rofi)
 data <- rofi::import_data(test = TRUE)
 names <- c("swetrau","fmp","atgarder","problem","kvalgranskning2014.2017")
 names(data) <- names
-combined.dataset <- rofi::merge_data(datagli)
-combined.dataset$ofi <- rofi::create_ofi(combined.dataset)
+combined.dataset <- rofi::merge_data(data)
 
-# Clean data 
-source("clean_all_predictors.R")
+## Create OFI column
+combined.dataset$ofi <- rofi::create_ofi(combined.dataset)
+combined.dataset <- clean_all_predictors(combined.dataset)
 
 #packages 
 library(dplyr)
@@ -18,14 +18,28 @@ library(boot)
 library(table1)
 
 
-#tar bort under 15 år
-combined.dataset_older_than_14 <- combined.dataset[combined.dataset$"pt_age_yrs" >= 15, ]
+## Separate and store cases without unknown outcome (OFI)
+missing.outcome <- is.na(combined.dataset$ofi)
+combined.dataset <- combined.dataset[!missing.outcome,]
 
-# Sorterar bort alla som inte har NISS över 15 eller ISS över 9
-Combined_dataset_inclusion <- combined.dataset_older_than_14[!(combined.dataset_older_than_14$"NISS" >= 15 | combined.dataset_older_than_14$"ISS" >= 9), ]
+## remove patients < 15 years
+combined.dataset <- combined.dataset[combined.dataset$pt_age_yrs > 14,]
+
+## Fix formating and remove wrong values like 999
+source("clean_all_predictors.R")
+combined.dataset <- clean_all_predictors(combined.dataset)
+
+## clean Audit filters
+combined.dataset <- clean_audit_filters(combined.dataset)
+
+# Clean data 
+source("clean_all_predictors.R")
+
+
+# Sorterar bort alla som inte har NISS över 15 eller ISS över 
+Combined_dataset1<- combined.dataset[(combined.dataset$"NISS" >= 15 | combined.dataset$"ISS" >= 9), ]
 
 #Gör om alla 999 till NA med apply 
-Combined_dataset_inclusion[Combined_dataset_inclusion == 999] <- NA
 
 
 # Skapa kohorter
@@ -37,6 +51,45 @@ Blunt_multisystem <- subset(Combined_dataset_inclusion, AIS > 30 & sex == "male"
 df$age_gt_30_label <- ifelse(df$age_gt_30, "yes", "no")
 
 
+#Penetrating trauma: At least one AIS ≥ 3 injury in any of the following AIS body regions: neck, thorax, and abdomen.
 
+is_serious <- function(code) {
+  severity <- substr(as.character(code), 8, 8)
+  as.numeric(severity) >= 3  
+}
+#if severity level is 3 or higher, the injury is considered serious
+number_of_regions <- function(code) {
+  region <- substr(as.character(code), 1, 1)
+  length(unique(region))
+}
 
+neck_chest_abdomen_region <- function(code) {
+  code <- code[!is.na(code)]
+  region <- substr(as.character(code), 1, 1)  
+  is.element(region,3:5)
+ 
+  }
+  
+#generates TRUE/FALSE
+  serious_neck_chest_abdomen_injury <- function(code) {
+    code <- code[!is.na(code)]
+    serious_injury <- is_serious(code)
+    sum(neck_chest_abdomen_region(code[serious_injury]) == TRUE )
+  }
+ 
+#generates numerical value
+  known_dominant_injury_pen <-fewer_variables[!is.na(fewer_variables$inj_dominant), ] 
+  known_dominant_injury_pen$penetrating <- NA
+  
+#removing na in dominant injury, new df
+  for (i in 1:nrow(known_dominant_injury_pen)) {
+    v = 0+i
+    if(serious_neck_chest_abdomen_injury(known_dominant_injury_pen[v,grepl( "AISCode" , names(known_dominant_injury_pen))]) >= 1 && known_dominant_injury_pen[v, "inj_dominant"] == 2) {
+      known_dominant_injury_pen[v,"penetrating"] <- TRUE
+    } else {
+      known_dominant_injury_pen[v,"penetrating"] <- FALSE
+    }
+  }
+ 
+#penetrating <- TRUE/FALSE
 
